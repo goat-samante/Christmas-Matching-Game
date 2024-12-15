ebc665473b82cb0f7714b2358a5fc8ac10d8102c
Samuel Amante <amantsa76@gmail.com>
2024-12-15 09:47:12 -0600

    Initial Commit

---
+++ Christmas Matching Game/Christmas_Matching_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Christmas_Matching_GameApp.swift
+//  Christmas Matching Game
+//
+//  Created by Samuel Amante on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Christmas_Matching_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
