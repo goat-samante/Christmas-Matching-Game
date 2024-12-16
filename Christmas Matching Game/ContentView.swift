5e70f72c9017b2655d89d40cb8c286d754626e14
Samuel Amante <amantsa76@gmail.com>
2024-12-16 09:59:23 -0600

    changed code a little and added some messages about what it means

--- Christmas Matching Game/ContentView.swift
+++ Christmas Matching Game/ContentView.swift
@@ -9,51 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            ZStack{
-                Image("Snowman")
+        NavigationStack {
+            ZStack {
+                Image("Christmas Tree Indoor")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .edgesIgnoringSafeArea(.all)
-                
-                CustomText(text: "Christmas Matching Game")
-                    .foregroundColor(.red)
-                    .font(.largeTitle).bold()
-                    .background(Color.mint)
-                    .position(x:218,y:350)
-                    .cornerRadius(10)
-                
                 VStack {
+                    Spacer()
+                    CustomText(text: "Christmas Matching Game")
+                        .foregroundColor(.red)
+                        .font(.largeTitle).bold()
+                        .background(Color.mint)
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                    
                     NavigationLink(destination: SecondScreen()) {
-                        
                         CustomText(text: "Enter Game")
                             .padding()
-                            .font(.largeTitle).bold()
                             .background(Color.black)
                             .cornerRadius(10)
-                            .position(x: 218, y: 750)
                     }
+                    Spacer()
                 }
             }
         }
     }
 }
+
 struct CustomText: View {
     let text: String
     var body: some View {
-        Text(text).font(Font.custom("Marker Felt", size: 33))
+        Text(text)
+            .font(Font.custom("Marker Felt", size: 33))
+            .font(.largeTitle).bold()
             .foregroundColor(.white)
             .background(Color.black)
             .cornerRadius(10)
     }
 }
-struct CustomTextButton: View {
-    let text: String
-    var body: some View {
-        Text(text).font(Font.custom("Marker Felt", size: 36))
-            .foregroundColor(.black)
-    }
-}
+
 #Preview {
     ContentView()
 }
+
+
--- Christmas Matching Game/SecondScreen.swift
+++ Christmas Matching Game/SecondScreen.swift
@@ -8,134 +8,105 @@
 import SwiftUI
 
 struct SecondScreen: View {
-    @State private var items = ["🎄", "🎅🏻", "🎁", "☃️", "❄️", "🛷"] // cards for game
+    @State private var items = ["🎄", "🎅🏻", "🎁", "☃️", "❄️", "🛷", "⛷️","🍪","🥛"] // christmas themed items for the game
     @State private var rearrangedItems: [String] = []
     @State private var selectedItems: [Int] = []
     @State private var matchedItems: [Int] = []
     @State private var gameWon: Bool = false
+    
     var body: some View {
         ZStack {
-            Image("Christmas Tree Indoor") // added chirstmas tree background image
-                .resizable()
+            Image("Snowman") // snowman background image
+                .resizable() // properties for the image
                 .aspectRatio(contentMode: .fill)
                 .edgesIgnoringSafeArea(.all)
-            Text("Placeholder") // title for the christmas game
-                .font(.largeTitle).bold()
-                .background(Color.white)
-                .position(x: 379, y:170)
-                .padding()
-            GridStack(rows: 2, columns: 6) { row, col in
-                let index = row * 6 + col
-                if index < rearrangedItems.count {
-                    return AnyView(
-                        ItemView(symbol: self.rearrangedItems[index], isFlipped: self.selectedItems.contains(index) ||
-                        self.matchedItems.contains(index))
-                        .onTapGesture {
-                            withAnimation(.smooth) {
-                                self.cardTapped(index: index)
-                            }
-                        }
-                    )
-                } else {
-                    return AnyView(EmptyView())
-                }
-            }
-            .padding()
-            .background(RoundedRectangle(cornerRadius: 20).stroke(Color.red,lineWidth: 4))
-            .padding()
-            Button("Restart") {
-                withAnimation(.smooth) {
-                    self.restartMatch()
-                }
-            }
-            .font(.largeTitle)
-            .background()
-            .background(Color.white)
-            .foregroundStyle(.green)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .shadow(radius: 10)
-            .position(x: 400, y: 600)
-            }
-        .onAppear(perform: rearrangeItems)
-        .alert(isPresented: self.$gameWon) {
-            Alert(title: Text("Winner"), message: Text("YOU BEAT THE GAME"), dismissButton: .default(Text("Play Again"), action: {
-                withAnimation(.smooth) {
-                    self.restartMatch()
-                }
-            }))
-        }
-        }
-        func rearrangeItems() {
-            rearrangedItems = (items + items).shuffled()
-        }
-        func cardTapped(index: Int) {
-            if selectedItems.count == 2 {
-                selectedItems.removeAll()
-            }
-            
-            if !matchedItems.contains(index) {
-                selectedItems.append(index)
-                if selectedItems.count == 2 {
-                    checkForWinner()
-                }
-            }
-        }
-        func checkForWinner() {
-            let firstIndex = selectedItems[0]
-            let secondIndex = selectedItems[1]
-            if rearrangedItems[firstIndex] == rearrangedItems[secondIndex] {
-                matchedItems += selectedItems
-                if matchedItems.count == rearrangedItems.count {
-                    gameWon = true
-                }
-            }
-        }
-        func restartMatch() {
-            matchedItems.removeAll()
-            selectedItems.removeAll()
-            rearrangeItems()
-        }
-    }
-    
-    #Preview {
-        SecondScreen()
-    }
-    struct ItemView: View {
-        var symbol: String
-        var isFlipped: Bool
-        
-        var body: some View {
-            ZStack {
-                Rectangle()
-                    .fill(isFlipped ? Color.red : Color.green)
-                    .frame(width: 50, height: 70)
-                    .clipShape(RoundedRectangle(cornerRadius: 10)).shadow(radius: 5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray, lineWidth: 2)
-                    )
-                if isFlipped {
-                    Text(symbol)
-                        .font(.largeTitle)
-                        .transition(.scale)
-                }
-            }
-        }
-    }
-    struct GridStack: View {
-        var rows: Int
-        var columns: Int
-        let content: (Int, Int) -> AnyView
-        
-        var body: some View {
-            VStack(spacing: 10) {
-                ForEach(0 ..< rows, id: \.self) { row in
+            CustomText(text: "Christmas Matching") // Title for the game, the text uses properties from the content view to make it uniform and look nice
+                .padding(.bottom, 600)
+            VStack(spacing: 10) { // grid settings for the matching game which makes it 18 squares total or 6 x 3. It includes settings regarding the spacing between each item, color of the circle before and after flipping it,
+                ForEach(0..<3, id: \.self) { row in
                     HStack(spacing: 10) {
-                        ForEach(0..<self.columns, id: \.self) { column in
-                            content(row, column)
+                        ForEach(0..<6, id: \.self) { col in
+                            let index = row * 6 + col
+                            if index < rearrangedItems.count {
+                                ZStack {
+                                    Circle()
+                                        .fill(selectedItems.contains(index) || matchedItems.contains(index) ? Color.red : Color.green)
+                                        .frame(width: 50, height: 70)
+                                        .clipShape(Rectangle())
+                                    if selectedItems.contains(index) || matchedItems.contains(index) {
+                                        Text(rearrangedItems[index])
+                                            .font(.largeTitle)
+                                            .transition(.scale)
+                                    }
+                                }
+                                .onTapGesture { // when tapping the circle it changes to the other side, also the animation makes it look cleaner.
+                                    withAnimation(.smooth) {
+                                        cardTapped(index: index)
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
+            Button("Restart") { // restart button which makes the card reshuffle
+                withAnimation(.smooth) {
+                    restartMatch()
+                }
+            }
+            //  Font, color, positioning, and background for the restart button
+            .font(Font.custom("Marker Felt", size: 33))
+            .font(.largeTitle).bold()
+            .foregroundColor(.white)
+            .background(Color.black)
+            .cornerRadius(10)
+            .padding(.top, 600)
+        }
+        // tells us that the game is finished with a title that states winner along with a little message. Below that there is a play again button to reset the game if you want to play again.
+        .onAppear(perform: rearrangeItems)
+        .alert(isPresented: self.$gameWon) {
+            Alert(
+                title: Text("Winner"),
+                message: Text("YOU BEAT THE GAME"),
+                dismissButton: .default(Text("Play Again"), action: {
+                    withAnimation(.smooth) {
+                        restartMatch()
+                    }
+                })
+            )
         }
     }
+    func rearrangeItems() {
+        rearrangedItems = (items + items).shuffled()
+    }
+    func cardTapped(index: Int) {
+        if selectedItems.count == 2 {
+            selectedItems.removeAll()
+        }
+        if !matchedItems.contains(index) { // says that if there are two matches it will see if they match and then
+            selectedItems.append(index)
+            if selectedItems.count == 2 {
+                checkForWinner()
+            }
+        }
+    }
+    func checkForWinner() { // states that if all the items are found / matched the game wll end
+        let firstIndex = selectedItems[0]
+        let secondIndex = selectedItems[1]
+        if rearrangedItems[firstIndex] == rearrangedItems[secondIndex] {
+            matchedItems += selectedItems
+            if matchedItems.count == rearrangedItems.count {
+                gameWon = true
+            }
+        }
+    }
+    func restartMatch() { // function for restart button that removes all the existing matches and locations in the grid. It also reshuffles the items aswell.
+        matchedItems.removeAll()
+        selectedItems.removeAll()
+        rearrangeItems()
+    }
+}
+
+#Preview {
+    SecondScreen()
+}
